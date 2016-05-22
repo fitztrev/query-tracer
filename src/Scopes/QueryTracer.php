351fff26cec4962e414864fd9c376923a94ace28
Percy Mamedy <percymamedy@users.noreply.github.com>
2016-05-22 17:30:05 +0400

    Use `DIRECTORY_SEPARATOR` for support on windows (#6)

--- src/Scopes/QueryTracer.php
+++ src/Scopes/QueryTracer.php
@@ -28,7 +28,7 @@ class QueryTracer implements Scope
 
         foreach ($traces as $trace) {
             // Find the first non-vendor-dir file in the backtrace
-            if (isset($trace['file']) && ! str_contains($trace['file'], '/vendor/')) {
+            if (isset($trace['file']) && ! str_contains($trace['file'], DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR)) {
                 $file = '"query.file" <> "' . $trace['file'] . '"';
                 $line = '"query.line" <> "' . $trace['line'] . '"';
 
